30c9b824ce2501956d67986b18b18668b812fff2
Almir666 <106429048+Almir666@users.noreply.github.com>
2024-12-13 17:05:21 +0300

    не заходит в Config

--- src/main/java/org/spring/oauth2/controller/HomeController.java
+++ src/main/java/org/spring/oauth2/controller/HomeController.java
@@ -1,8 +1,6 @@
 package org.spring.oauth2.controller;
 
-
 import lombok.AllArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
